d2012cd81c767f6833f669d8dca19ee4d62f1f84
Haro Abdulah <148715232+Haro-95@users.noreply.github.com>
2025-07-18 18:36:22 +0300

    Remove GitHub repository link from footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -13,27 +13,13 @@ const Copyright = styled.p`
   margin: 0.5rem 0;
 `;
 
-const GitHubLink = styled.a`
-  color: #6366f1;
-  text-decoration: none;
-  font-weight: 500;
-  
-  &:hover {
-    text-decoration: underline;
-  }
-`;
-
 const Footer: React.FC = () => {
   const currentYear = new Date().getFullYear();
   
   return (
     <FooterContainer>
-      <Copyright>© {currentYear} ImageShrink - Free, fast image compression</Copyright>
-      <div>
-        <GitHubLink href="https://github.com/Haro-95/image-shrink" target="_blank" rel="noopener noreferrer">
-          View on GitHub
-        </GitHubLink>
-      </div>
+      <Copyright> {currentYear} ImageShrink - Free, fast image compression</Copyright>
+
     </FooterContainer>
   );
 };
